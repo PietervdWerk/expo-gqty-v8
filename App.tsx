66ba9a4fb4f7564d42fb963361109a296474f6d2
Pieter van der Werk <pieter.vdwerk@gmail.com>
2023-11-02 00:09:36 +0100

    add v8 and gqty startwars

--- App.tsx
+++ App.tsx
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, Text, TextInput } from 'react-native'
+import { useQuery } from './gqty'
+import { Suspense, useState } from 'react'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [searchInput, setSearchInput] = useState(100)
+  const { allPeople } = useQuery({ suspense: false })
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  return (
+    <Suspense fallback={<Text>Loading...</Text>}>
+      <ScrollView style={{ padding: 80 }}>
+        <TextInput
+          value={searchInput.toString()}
+          onChangeText={(e) => {
+            try {
+              const nmbr = parseInt(e)
+              if (isNaN(nmbr)) return
+              setSearchInput(nmbr)
+            } catch (e) {}
+          }}
+          placeholder='Search for a person'
+          style={{
+            padding: 10,
+            backgroundColor: '#eee',
+            borderRadius: 5,
+            marginBottom: 10,
+            borderWidth: 1,
+          }}
+        />
+        {allPeople({ first: searchInput })?.people?.map((person) => (
+          <Text key={person?.name}>{person?.name}</Text>
+        ))}
+      </ScrollView>
+    </Suspense>
+  )
+}
